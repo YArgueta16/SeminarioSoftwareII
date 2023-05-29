e6710cea99f73f60e32dccf756135c50ddbdbc6d
YArgueta16 <84729560+YArgueta16@users.noreply.github.com>
2023-05-28 20:47:35 -0600

    update teams
    tarea STS

---
+++ src/libs/teams/teams.ts
@@ -0,0 +1,47 @@
+export interface ITeams {
+    _id?: string;
+    name: string;
+    members: string;
+    isActive: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
+  
+  // const newProject: Required<IProject> = {};
+  const memoryTeams: ITeams[] = [];
+  let createdTeams = 0;
+  
+  export const createTeam = async (team: ITeams) => {
+    const newTeam = { ...team };
+    newTeam._id = (++createdTeams).toString();
+    newTeam.createdAt = new Date();
+    newTeam.updatedAt = newTeam.createdAt;
+    memoryTeams.push(newTeam);
+    return newTeam;
+  }
+
+  export const getTeams = async () => {
+    return memoryTeams;
+  };
+
+  export const getTeam = async (id:string) =>{
+    const team = memoryTeams.find(p => p._id === id);
+    if(!team) throw new Error('Team not found');
+    return team;
+  }
+
+  export const updateTeam = ( id:string, team:Partial<ITeams>) => {
+    
+    const index = memoryTeams.findIndex(p => p._id === id);
+    if (index === -1) throw new Error('Team not found');
+    memoryTeams[index] = { ...memoryTeams[index], ...team, updatedAt: new Date() };
+    return memoryTeams[index];
+  }
+
+  export const deleteTeam = (id:string) => {
+    const index = memoryTeams.findIndex(p => p._id === id);
+    if (index === -1) throw new Error('Team not found');
+    memoryTeams.splice(index, 1);
+    return true;
+  }
+  
--- src/routes/api/teams/index.ts
+++ src/routes/api/teams/index.ts
@@ -1,8 +1,65 @@
 import express from 'express';
-const router  = express.Router();
+const router = express.Router();
+
+import { createTeam, deleteTeam, getTeams, updateTeam, getTeam} from '@libs/teams/teams';
 
 router.get('/', (_req, res) => {
-    res.json({version:1, scope:'teams'});
-   });
+  res.json({version:1, scope:'projects'});
+});
+
+//  /api/projects/echo/hola?variable1=a&variable2=b
+
+
+router.get('/all', async (_req, res) => {
+  try{
+    const projects = await getTeams();
+    res.json(projects);
+  }
+  catch(ex:any ){
+    return res.status(500).json({error: ex?.message});
+  }
+});
+
+router.post('/new', async (req, res) => {
+  try{
+    const { name = '', members = '', isActive = false } = req.body;
+    const newTeam = { name, members, isActive: (isActive && true) };
+    const createdTeam = await createTeam(newTeam);
+    res.json(createdTeam);}
+  catch(ex:any ){
+    return res.status(500).json({error: ex?.message});
+
+  }
+});
+router.put ('/upd/:id', async (req, res) =>{
+   try{
+    const {id= ''} = req.params;
+    const {name= '', members = '', isActive= false} = req.body;
+    const updatedTeam = await updateTeam(id,{name,members,isActive:(isActive&&true)});
+    return res.json(updatedTeam); }
+    catch(ex:any ){
+      return res.status(500).json({error: ex?.message});
+    }
+  });
+  router.delete('/del/:id', async (req, res) => {
+    try{
+      const {id=''} = req.params;
+      const deletedProject = await deleteTeam(id);
+      res.json({deleted: deletedProject, id});}
+    catch(ex:any ){
+      return res.status(500).json({error: ex?.message});
+    }
+  });
+
+  router.get('/byid/:id',async (req, res) => {
+    try{
+      const {id =''} = req.params;
+      const project = await getTeam(id);
+      return res.json(project);
+    }catch(ex:any ){
+      return res.status(500).json({error: ex?.message});
+    }
+
+  });
 
 export default router;
