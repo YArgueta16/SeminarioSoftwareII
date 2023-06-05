3cccbf567478eeb691018d9d0697e5058419c5b0
YArgueta16 <84729560+YArgueta16@users.noreply.github.com>
2023-06-05 00:25:54 -0600

    Update Return

--- src/routes/api/teams/index.ts
+++ src/routes/api/teams/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 import { createTeam, deleteTeam, getTeams, updateTeam, getTeam} from '@libs/teams/teams';
 
 router.get('/', (_req, res) => {
-  res.json({version:1, scope:'projects'});
+  return res.json({version:1, scope:'projects'});
 });
 
 //  /api/projects/echo/hola?variable1=a&variable2=b
@@ -13,7 +13,7 @@ router.get('/', (_req, res) => {
 router.get('/all', async (_req, res) => {
   try{
     const projects = await getTeams();
-    res.json(projects);
+    return res.json(projects);
   }
   catch(ex:any ){
     return res.status(500).json({error: ex?.message});
@@ -22,10 +22,10 @@ router.get('/all', async (_req, res) => {
 
 router.post('/new', async (req, res) => {
   try{
-    const { name = '',description='', members = '', owner='', status = false } = req.body;
-    const newTeam = { name, description,members,owner, status: (status && true) };
-    const createdTeam = await createTeam(newTeam);
-    res.json(createdTeam);}
+        const { name = '',description='', members = '', owner='', status = false } = req.body;
+        const newTeam = { name, description,members,owner, status: (status && true) };
+        const createdTeam = await createTeam(newTeam);
+        return res.json(createdTeam);}
   catch(ex:any ){
     return res.status(500).json({error: ex?.message});
 
@@ -33,10 +33,10 @@ router.post('/new', async (req, res) => {
 });
 router.put ('/upd/:id', async (req, res) =>{
    try{
-    const {id= ''} = req.params;
-    const {name = '',description='', members = '', owner='', status = false } = req.body;
-    const updatedTeam = await updateTeam(id,{name,description,members,owner,status:(status&&true)});
-    return res.json(updatedTeam); }
+        const {id= ''} = req.params;
+        const {name = '',description='', members = '', owner='', status = false } = req.body;
+        const updatedTeam = await updateTeam(id,{name,description,members,owner,status:(status&&true)});
+        return res.json(updatedTeam); }
     catch(ex:any ){
       return res.status(500).json({error: ex?.message});
     }
@@ -45,7 +45,7 @@ router.put ('/upd/:id', async (req, res) =>{
     try{
       const {id=''} = req.params;
       const deletedProject = await deleteTeam(id);
-      res.json({deleted: deletedProject, id});}
+      return res.json({deleted: deletedProject, id});}
     catch(ex:any ){
       return res.status(500).json({error: ex?.message});
     }
